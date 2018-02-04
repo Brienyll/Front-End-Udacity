5370711a6193648a10bf400424f311900f8cf787
Brienyll <brienyll29@gmail.com>
2018-02-04 11:28:17 -0800

    Quiz 4-2

--- juiceBottle.js
+++ juiceBottle.js
@@ -12,18 +12,13 @@
 var num = 99;
 
 while (num > 0) {
+    // check value of num
     if (num === 1) {
         console.log( num +" bottle of juice on the wall! " +num+ " bottle of juice! Take one down, pass it around... "+ (num - 1) +" bottles of juice on the wall!");
     }  else if (num === 2){
         console.log( num +" bottles of juice on the wall! " +num+ " bottles of juice! Take one down, pass it around... "+ (num - 1) +" bottle of juice on the wall!");
     }   else { 
-        // check value of num
         console.log(num + " bottles of juice on the wall! " +num+" bottles of juice! Take one down, pass it around... "+ (num - 1)+" bottles of juice on the wall!");
-        // print lyrics using num
-        
-        // don't forget to check pluralization on the last line!
-        
-        // decrement num
     }
     num -- ;
 }
