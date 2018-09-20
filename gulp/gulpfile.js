95008b7ba6c7fedec6e7c8c34d922effa3e29c11
Brix <brienyll29@gmail.com>
2018-09-20 15:34:59 -0700

    Add files via upload

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,5 +1,25 @@
-let gulp = require("gulp");
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 
-gulp.task("default", function(){
-    console.log("Hello gulp!")
+gulp.task("default", ["styles"], function() {
+  gulp.watch("sass/**/*.scss", ["styles"]);
+
+  browserSync.init({
+    server: "./"
+  });
+});
+
+gulp.task("styles", function() {
+  gulp
+    .src("sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 });
